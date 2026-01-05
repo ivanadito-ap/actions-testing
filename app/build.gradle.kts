6bd0a8ee8b98fe380fb0611e046ac6b6139c0b06
ivanadito-ap <ivanaditoap@gmail.com>
2026-01-05 09:29:16 +0700

    Fix build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,3 +41,9 @@ tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = application.mainClass.get()
+    }
+}
